e7b4e70b3e59bf88b3adf4ac6c0028fa345e24cb
David-Lor <17401854+david-lor@users.noreply.github.com>
2019-06-17 16:56:34 +0200

    Improve Setup

--- setup.py
+++ setup.py
@@ -2,17 +2,26 @@
 
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="pybuses-entities",
-    version="0.1.0",
+    version="0.0.1",
     description="Data classes to work with Stops and Buses/vehicles on a public transportation network",
-    long_description="The goal of PyBusEnt is to provide a set of data classes that help organizing, managing and "
-                     "working with the Stops and Buses on a public transportation network backend or frontend "
-                     "Python project. This project was mainly focused on a Telegram Bot backend and an API to provide "
-                     "all the data required (Stops and Buses coming to these stops).",
     author="David Lorenzo",
     url="https://github.com/David-Lor/PyBuses",
-    packages=("pybuses_entities",),
+    packages=("pybusent",),
     install_requires=("pydantic",),
-    # test_suite="tests.run_all_tests"
+    license="Apache 2.0",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ),
+    long_description_content_type="text/markdown",
+    long_description=long_description,
 )
